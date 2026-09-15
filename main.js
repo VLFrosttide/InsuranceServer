e729c6634d94895d7cf0abacdc00cb5964e67929
VLFrosttide <shacxd@gmail.com>
2026-09-15 05:37:22 +0300

    awd
    awd

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 const express = require("express");
 const app = express();
-const port = 443;
+const port = process.env.PORT || 8080;
 const http = require("http");
 const path = require("path");
 const httpServer = http.createServer(app);
@@ -11,4 +11,12 @@ const db = new DatabaseSync("./Database.db");
 
 app.post("/", (req, res) => {
   res.send("Got a POST request");
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+httpServer.listen(port, "0.0.0.0", () => {
+  console.log(`Server listening on port ${port}`);
 });
