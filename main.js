0231f9a6fa30018bf143fad834cd8ff9b8083298
VLFrosttide <shacxd@gmail.com>
2026-09-15 05:05:57 +0300

    asedz
    asg

--- main.js
+++ main.js
@@ -1,52 +1,10 @@
 "use strict";
-//Starts with: npm start
+const express = require("express");
+const app = express();
+const port = 5500;
+const http = require("http");
+const path = require("path");
+const httpServer = http.createServer(app);
+const { DatabaseSync } = require("node:sqlite");
 
-import {
-  app,
-  BrowserWindow,
-  globalShortcut,
-  screen,
-  ipcMain,
-  Menu,
-  nativeTheme,
-  dialog,
-} from "electron";
-import path from "path";
-let win;
-let MenuTemp;
-let PreloadPath = path.join(app.getAppPath(), "/renderer/preload.js");
-
-const CreateWindow = () => {
-  win = new BrowserWindow({
-    width: 800,
-    height: 550,
-    x: 490,
-    y: 0,
-    title: "Reroll",
-
-    webPreferences: {
-      nodeIntegration: false,
-      sandbox: true,
-      contextIsolation: true,
-      preload: PreloadPath,
-      contentSecurityPolicy:
-        "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self'; font-src 'self'; connect-src 'self'; manifest-src 'self';",
-    },
-  });
-
-  win.loadFile("renderer/index.html");
-};
-
-app.whenReady().then(() => {
-  CreateWindow();
-  MenuTemp = [
-    { label: "Reload", accelerator: "Ctrl+R", role: "forceReload" },
-    {
-      label: "Dev tools",
-      role: "toggleDevTools",
-      accelerator: "Ctrl+`",
-    },
-  ];
-  const menu = Menu.buildFromTemplate(MenuTemp);
-  Menu.setApplicationMenu(menu);
-});
+const db = new DatabaseSync("./Database.db");
