8bca991b43cb3b3a1dd76b17b2f75c07e4bc8170
VLFrosttide <shacxd@gmail.com>
2026-09-15 05:20:14 +0300

    awd
    awd

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 "use strict";
 const express = require("express");
 const app = express();
-const port = 5500;
+const port = 443;
 const http = require("http");
 const path = require("path");
 const httpServer = http.createServer(app);
 const { DatabaseSync } = require("node:sqlite");
 
 const db = new DatabaseSync("./Database.db");
+
+app.post("/", (req, res) => {
+  res.send("Got a POST request");
+});
