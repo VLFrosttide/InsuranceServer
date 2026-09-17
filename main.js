4c9a1823c9ea0e4dc8aea8d96f64bb54a20f0d90
VLFrosttide <shacxd@gmail.com>
2026-09-17 04:15:58 +0300

    Revert "awd"
    This reverts commit 46a4f54fca212fd44deb6d342eb45cbd445ba857.

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 "use strict";
-import express from "express";
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-import http from "http";
+const http = require("http");
+// const path = require("path");
 const httpServer = http.createServer(app);
-import { Client } from "pg";
+const { Client } = require("pg");
 const DBClient = new Client(process.env.ConString);
-import Bcrypt from "bcrypt";
-import Crypto from "crypto";
+const Bcrypt = require("bcrypt");
+const Crypto = require("crypto");
 await DBClient.connect();
 app.post("/logme", async (req, res) => {
   res.send("Got a POST request");
